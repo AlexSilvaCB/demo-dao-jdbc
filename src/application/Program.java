28be03016ce86a75f77d2faa203471523112288c
AlexSilvaCB <96430292+AlexSilvaCB@users.noreply.github.com>
2023-04-07 20:42:41 -0300

    SellerDaoJDBC and DaoFactory

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,5 +1,7 @@
 package application;
 
+import model.Dao.DaoFactory;
+import model.Dao.SellerDao;
 import model.entities.Department;
 import model.entities.Seller;
 
@@ -11,7 +13,9 @@ public class Program{
 
         Department obj = new Department(1, "Books");
         Seller seller = new Seller(21, "Bob", "bob@gmail.com.br", new Date(),3000.0, obj);
-        System.out.println(obj);
+
+        SellerDao sellerDao = DaoFactory.createSellerDao();
+
         System.out.println(seller);
     }
 }
